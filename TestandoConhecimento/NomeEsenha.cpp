d2fa599ed90d4f1a4c30e7aaef21ed444ba944e7
Otavio <correaotavio0409@gmail.com>
2024-07-24 15:34:48 -0300

    Class Nome e senha

---
+++ TestandoConhecimento/NomeEsenha.cpp
@@ -0,0 +1,8 @@
+#include "NomeEsenha.h"
+
+
+NomeEsenha::NomeEsenha() : nomeApp(""), senhaApp("") {};
+
+NomeEsenha::NomeEsenha(const std::string& nomeApp,const std::string& senhaApp) : nomeApp(nomeApp), senhaApp(senhaApp) {};
+
+
---
+++ TestandoConhecimento/NomeEsenha.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class NomeEsenha
+{
+private:
+	std::string nomeApp;
+	std::string senhaApp;
+
+public:
+	NomeEsenha();
+	NomeEsenha(const std::string& nomeApp, const std::string& senhaApp);
+
+
+
+
+};
